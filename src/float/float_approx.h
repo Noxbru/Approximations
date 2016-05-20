434349e4d07f200165bba8badb2da28f670f4f82
Bruno Jiménez <brunojimen@gmail.com>
2016-05-20 19:35:46 +0100

    Add a header to access all functions

---
+++ src/float/float_approx.h
@@ -0,0 +1,14 @@
+#ifndef FLOAT_APPROX_H
+#define FLOAT_APPROX_H
+#pragma once
+
+#include "power/sqrtf.h"
+#include "power/cbrtf.h"
+#include "power/rsqrtf.h"
+
+#include "exponential/logf.h"
+#include "exponential/log1pf.h"
+
+#include "error/erff.h"
+
+#endif /* end of include guard: FLOAT_APPROX_H */
